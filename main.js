c1536e1da0b438fe1b7d8e177214ec8b1c64f4f9
htylerbrooks <htb5qh@gmail.com>
2015-06-08 23:37:35 -0400

    added search functionality/need to figure out how to clear div after each search

--- main.js
+++ main.js
@@ -4,22 +4,22 @@ $(document).ready(function(){
   $('form#search input[type=submit]').on("click", searchOpenBeerDB);
 });
 
-var keyword = $("#search-keyword").val();
-
-var url = 'http://api.brewerydb.com/v2/search?q=coors&type=beer&key=aae5c8039d40d4cad7ada705c75745d2';
 
 function searchOpenBeerDB(event){
 
   event.preventDefault();
 
-  var container = $("#results");
+var term = $("#search-term").val();
+var url = 'http://api.brewerydb.com/v2/search?q='+term+'&type=beer&key=aae5c8039d40d4cad7ada705c75745d2';
+var container = $("#results");
 
   $.ajax({
     url: url,
     method:"get",
     dataType: "json"
   }).done(function(result) {
-      container.append(result["data"][1]["name"]);
+      container.append(result["data"][1]["description"]);
+
     // resultsDisplay(result);
   });
 
