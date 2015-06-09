f0f42fed3edad3767a42c65c66c000a736dd8f1a
htylerbrooks <htb5qh@gmail.com>
2015-06-09 13:39:17 -0400

    able to clear div with mouseover submit button/need to implement error handling

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
 $(document).ready(function(){
   console.log("Lets go!");
 
+  $('form#search input[type=submit]').on("mouseover", clearDiv);
   $('form#search input[type=submit]').on("click", searchOpenBeerDB);
 });
+
 
 
 function searchOpenBeerDB(event){
 
   event.preventDefault();
 
-var term = $("#search-term").val();
-var url = 'http://api.brewerydb.com/v2/search?q='+term+'&type=beer&key=aae5c8039d40d4cad7ada705c75745d2';
-var container = $("#results");
+  var term = $("#search-term").val();
+  var url = 'http://api.brewerydb.com/v2/search?q='+term+'&type=beer&key=aae5c8039d40d4cad7ada705c75745d2';
+  var container = $("#results");
 
   $.ajax({
     url: url,
     method:"get",
     dataType: "json"
   }).done(function(result) {
-      container.append(result["data"][1]["description"]);
+    container.append(result["data"][1]["description"]);
 
     // resultsDisplay(result);
   });
 
 }
 
-  // function resultsDisplay (result) {
-  //   var container = $("#results");
-  //   container.append(results.name + " Description: " + results.description)
-  // }
+function clearDiv(){
+  $("#results").empty();
+}
+
+// function resultsDisplay (result) {
+//   var container = $("#results");
+//   container.append(results.name + " Description: " + results.description)
+// }
